f16036222c9fcf064723b4b398c66f5751cb6315
Jason Benn <jasoncbenn@gmail.com>
2015-06-04 09:38:11 -0700

    Tricky transitioning to index from a delete action. Key: get the record's controller. Not the best solution, but /shrug.

--- rails-mastery-web/app/routes/features.js
+++ rails-mastery-web/app/routes/features.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    delete: function(feature) {
+      // works when invoked from features/index
+      // complains features.destroyRecord is not a thing when called from features/show
+      // why? they're both passing models. i can verify that... yup.
+      // they're both coming through this event handler.
+      // AH. from show, i'm passing a promise (?)
+      // from features.index, i'm passing a store object.
+      var _this = this;
+      feature.destroyRecord().then(function() {
+        console.log(_this)
+        _this.get('controller').transitionToRoute('features')
+      }, function() {
+        feature.rollback();
+        console.log('error callback!', arguments)
+      });
+      return false;
+    }
+  }
 });
--- rails-mastery-web/app/routes/features/index.js
+++ rails-mastery-web/app/routes/features/index.js
@@ -3,17 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.findAll('feature');
-  },
-
-  actions: {
-    delete: function(feature) {
-      feature.destroyRecord().then(function() {
-        console.log('success callback!', arguments)
-      }.bind(this), function() {
-        feature.rollback();
-        console.log('error callback!', arguments)
-      }.bind(this));
-      return false;
-    }
   }
 });
